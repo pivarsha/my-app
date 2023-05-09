5a30e35cd9a5450dbedfd7327a08cfc9680a555b
Varsha Rajput <varsha7022001@gmail.com>
2023-05-09 23:17:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://source.unsplash.com/random/200x200?sig=1" className="App-logo" alt="logo" />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
